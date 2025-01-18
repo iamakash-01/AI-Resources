ee26c892defae5b74c31628668a91b85d0685e40
AKASH <akashboro8080@gmail.com>
2025-01-18 21:05:09 +0530

    Add files via upload

---
+++ NLP/emailIdFetcher.py
@@ -0,0 +1,18 @@
+# Email ID fetcher using spacy library
+import spacy
+
+# load the blank spacy model
+nlp = spacy.blank("en")
+
+# text to be processed
+text = "My email is : akashboro111@gmail.com. And my friends email is : mpratim32@gmail.com.\
+    And another friends email is : prakash@gmail.com"
+doc = nlp(text)
+
+# list to store email ids
+emails = []
+for token in doc :
+    if token.like_email : 
+        emails.append(token.text)
+
+print(emails)
